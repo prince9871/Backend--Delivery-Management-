4d67836834f20bbff1a82efb265c38e760a8d24c
Prince Soni <soniprince142@gmail.com>
2024-12-02 12:47:31 +0530

    setup is in progress..

--- app.js
+++ app.js
@@ -27,4 +27,4 @@ app.get('/', (req, res) => {
 // app.use('/api/routes', routeRoutes);
 
 const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
